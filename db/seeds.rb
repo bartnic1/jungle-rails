7ee7fe13c786a6b2775da61ffd0e4685d66225e0
Piotr <bartnic1@gmail.com>
2018-04-03 03:41:02 +0000

    changing back to what it was before

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,8 @@ Product.destroy_all
 p1 = cat1.products.create!({
   name:  'Men\'s Classy shirt',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel1.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel1.jpg',
+  image: open_asset(apparel1.jpg),
   quantity: 10,
   price: 64.99
 })
@@ -46,7 +47,8 @@ p1 = cat1.products.create!({
 cat1.products.create!({
   name:  'Women\'s Zebra pants',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel2.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel2.jpg',
+  image: open_asset(apparel2.jpg),
   quantity: 18,
   price: 124.99
 })
@@ -54,7 +56,8 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Hipster Hat',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel3.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel3.jpg',
+  image: open_asset(apparel3.jpg),
   quantity: 4,
   price: 34.49
 })
@@ -62,7 +65,8 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Hipster Socks',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel4.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel4.jpg',
+  image: open_asset(apparel4.jpg),
   quantity: 8,
   price: 25.00
 })
@@ -70,7 +74,8 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Russian Spy Shoes',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel5.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel5.jpg',
+  image: open_asset(apparel5.jpg),
   quantity: 8,
   price: 1_225.00
 })
@@ -78,7 +83,8 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Human Feet Shoes',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel6.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/apparel6.jpg',
+  image: open_asset(apparel6.jpg),
   quantity: 82,
   price: 224.50
 })
@@ -87,7 +93,8 @@ cat1.products.create!({
 cat2.products.create!({
   name:  'Modern Skateboards',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/electronics1.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/electronics1.jpg',
+  image: open_asset(electronics1.jpg),
   quantity: 40,
   price: 164.49
 })
@@ -95,7 +102,8 @@ cat2.products.create!({
 cat2.products.create!({
   name:  'Hotdog Slicer',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/electronics2.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/electronics2.jpg',
+  image: open_asset(electronics2.jpg),
   quantity: 3,
   price: 26.00
 })
@@ -103,7 +111,8 @@ cat2.products.create!({
 cat2.products.create!({
   name:  'World\'s Largest Smartwatch',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/electronics3.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/electronics3.jpg',
+  image: open_asset(electronics3.jpg),
   quantity: 32,
   price: 2_026.29
 })
@@ -111,7 +120,8 @@ cat2.products.create!({
 p10 = cat3.products.create!({
   name:  'Optimal Sleeping Bed',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/furniture1.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/furniture1.jpg',
+  image: open_asset(furniture1.jpg),
   quantity: 320,
   price: 3_052.00
 })
@@ -119,7 +129,8 @@ p10 = cat3.products.create!({
 p11 = cat3.products.create!({
   name:  'Electric Chair',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/furniture2.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/furniture2.jpg',
+  image: open_asset(furniture2.jpg),
   quantity: 2,
   price: 987.65
 })
@@ -127,7 +138,8 @@ p11 = cat3.products.create!({
 p12 = cat3.products.create!({
   name:  'Red Bookshelf',
   description: Faker::Hipster.paragraph(4),
-  image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/furniture3.jpg',
+  # image: 'https://res.cloudinary.com/chronosphere111/image/upload/v1522716307/furniture3.jpg',
+  image: open_asset(furniture3.jpg),
   quantity: 23,
   price: 2_483.75
 })
