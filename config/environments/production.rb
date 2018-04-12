c87d747a13fe58f919ef101906e2ada8e4ce59c1
Piotr <bartnic1@gmail.com>
2018-04-12 20:58:54 +0000

    temp commit for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'bartnic1@gmail.com',
-    password:             'Royale13',
+    password:             'muonium',
     authentication:       'plain',
     enable_starttls_auto: true }
 
