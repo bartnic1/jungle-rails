7dd557a62aa1c7a0ea0298feb5d9317926c7f6bd
Piotr <bartnic1@gmail.com>
2018-04-12 19:39:09 +0000

    numerous config settings changed for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,14 @@ Rails.application.configure do
   # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'bartnic1',
+    password:             'Royale13',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
